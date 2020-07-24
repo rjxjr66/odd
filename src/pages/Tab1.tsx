3b788187cb9fc84ae093bacb88edf1934d217b72
beomgi <aa187523@gmail.com>
2020-07-24 20:48:18 +0900

    .

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -10,6 +10,7 @@ import { ParkingLot } from '../models/ParkingLot';
 import LocationService from '../services/location.service'
 import { Geolocation } from '@ionic-native/geolocation';
 import { menu, personCircle } from 'ionicons/icons';
+import { OddButton } from '../components/OddButton';
 
 const Tab1: React.FC = () => {
   const [showSearchbar, setShowSearchbar] = useState(false);
@@ -47,35 +48,37 @@ const Tab1: React.FC = () => {
   return (
     <IonPage className="Tab1">
       <IonContent>
-      <IonGrid>
-        <IonRow>
-          <IonCol>
-            <IonButton onClick={nearBySearch} expand="full">주변 검색</IonButton>
-          </IonCol>
-          <IonCol>
-            <IonButton onClick={() => setShowSearchbar(!showSearchbar)} expand="full">키워드 검색</IonButton>
-          </IonCol>
-        </IonRow>
-      </IonGrid>
+        <h1>어디대</h1>
+        <div className="buttons">
+          <OddButton onClick={nearBySearch}>주변 검색</OddButton>
+          <OddButton onClick={() => setShowSearchbar(!showSearchbar)}>키워드 검색</OddButton>
+        </div>
+        {
+          showSearchbar ? searchBar() : null
+        }
+        <ParkingList list={searchResult} removable={false} tab="1"></ParkingList>
+        <IonFab vertical="bottom" horizontal="start" slot="fixed">
+          <IonFabButton>
+            <IonIcon icon={menu} />
+          </IonFabButton>
+          <IonFabList side="end">
 
-      {
-        showSearchbar ? searchBar() : null
-      }
-      <ParkingList list={searchResult} removable={false} tab="1"></ParkingList>
-      <IonFab vertical="bottom" horizontal="start" slot="fixed">
-        <IonFabButton>
-          <IonIcon icon={menu} />
-        </IonFabButton>
-        <IonFabList side="end">
-          <IonFabButton routerLink="/reserve">예약</IonFabButton>
-          <IonFabButton routerLink="/mylot">등록</IonFabButton>
-          <IonFabButton routerLink="/mypage"><IonIcon icon={personCircle} /></IonFabButton>
-        </IonFabList>
-      </IonFab>
+            <IonFabButton routerLink="/reserve">예약</IonFabButton>
+            <IonFabButton routerLink="/mylot">등록</IonFabButton>
+            <IonFabButton routerLink="/mypage"><IonIcon icon={personCircle} /></IonFabButton>
 
-      <div className="ellipse Ellipse-12"></div>
-      <div className="ellipse Ellipse-2"></div>
-      <div className="ellipse Ellipse-5"></div>
+          </IonFabList>
+        </IonFab>
+
+        <div className="ellipse Ellipse-1"></div>
+        <div className="ellipse Ellipse-2"></div>
+        <div className="ellipse Ellipse-3"></div>
+        <div className="ellipse Ellipse-4"></div>
+        <div className="ellipse Ellipse-5"></div>
+        <div className="ellipse Ellipse-6"></div>
+        <div className="ellipse Ellipse-7"></div>
+        <div className="ellipse Ellipse-8"></div>
+        <div className="ellipse Ellipse-12"></div>
 
       </IonContent>
     </IonPage>
